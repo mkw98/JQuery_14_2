d7e8d0f53b1e14d65d5eed5e2e69e042b2604ad8
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-18 13:04:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ $(document).ready(function(){
 });
 var span = $('span');
     span.each(function(index, element) {
-});
-span.each(function(index, element) {
-	if(index % 2 == 0) {
+	  if(index % 2 == 0) {
 		$(element).css('color', 'red');
 	};
 });
